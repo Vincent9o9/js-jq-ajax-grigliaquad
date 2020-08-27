9070dbc796cd7b4b920e214f4a64e8173ee301e4
Vincent <23vincent86@gmail.com>
2020-08-27 16:17:53 +0200

    box

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert();
+$( document ).ready(function() {
+    //Ciclo for per clonazione quadrati
+    for (var i = 0; i < 36; i++) {
+        $('.center').append('<div class="box"></div>');
+    }
+
+});
